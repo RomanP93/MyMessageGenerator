e5290447feabc0b22a822135afb933d495ab519e
Roman <rpeter010755@gmail.com>
2026-04-21 11:52:28 +0100

    added 3 arrays and random pick in each of the array

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('ajmooo breee');
+const intro = ['Chef says:', 'Service alert:', 'Boss yells:'];
+const person = ['the new guy', 'the waiter', 'the pizza chef'];
+const action = ['needs more coffe', 'needs more beer', 'will burn the pan'];
+
+
+function generateMessage() {
+    let randomPickIntro = intro[Math.floor(Math.random() * intro.length)];
+    let randomPickPerson = person[Math.floor(Math.random() * person.length)];
+    let randomPickAction = action[Math.floor(Math.random() * action.length)];
+    console.log(randomPickIntro, randomPickPerson,randomPickAction);
+}
